f66ee539e2fe2d7daee8fa8d3a14a9246429a408
ANIEFIOK AKPAN <akp.ani@yahoo.com>
2020-10-25 00:38:19 +0100

    chore(test):add more test

--- src/main/java/com/semicolondevop/suite/model/techStack/TechStackConverter.java
+++ src/main/java/com/semicolondevop/suite/model/techStack/TechStackConverter.java
@@ -9,25 +9,25 @@ import java.util.stream.Stream;
  * @created 25/10/2020 - 12:12 AM
  * @project com.semicolondevop.suite.model.techStack in ds-suite
  */
-@Converter(autoApply = true)
-public class TechStackConverter implements AttributeConverter<TechStackType,String> {
-    @Override
-    public String convertToDatabaseColumn(TechStackType techStackType) {
-        if (techStackType == null) {
-            return null;
-        }
-        return techStackType.getCode();
-    }
-
-    @Override
-    public TechStackType convertToEntityAttribute(String code) {
-        if (code == null) {
-            return null;
-        }
-
-        return Stream.of(TechStackType.values())
-                .filter(c -> c.getCode().equals(code))
-                .findFirst()
-                .orElseThrow(IllegalArgumentException::new);
-    }
-}
+//@Converter(autoApply = true)
+//public class TechStackConverter implements AttributeConverter<TechStackType,String> {
+//    @Override
+//    public String convertToDatabaseColumn(TechStackType techStackType) {
+//        if (techStackType == null) {
+//            return null;
+//        }
+//        return techStackType.getCode();
+//    }
+//
+//    @Override
+//    public TechStackType convertToEntityAttribute(String code) {
+//        if (code == null) {
+//            return null;
+//        }
+//
+//        return Stream.of(TechStackType.values())
+//                .filter(c -> c.getCode().equals(code))
+//                .findFirst()
+//                .orElseThrow(IllegalArgumentException::new);
+//    }
+//}
